93f75ff9ddf08ddb17b792c201fa133df9c2627a
Cheney <cheney.chiu@outlook.com>
2019-04-14 20:14:21 +0800

    commit load and train file

---
+++ get_load.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+import cv2
+
+
+def get_data(images_path, labels_path, img_h, img_w, nb_class, nb_channel, scale_list, mode='train'):
+    print('-' * 10 + 'loading data')
+    assert (nb_class >= 2)
+    images = os.listdir(images_path)
+    images.sort()
+    labels = os.listdir(labels_path)
+    labels.sort()
+    shape = []
+    for i in range(len(images)):
+        image = cv2.imread(os.path.join(images_path, images[i]))
+        height, width, _ = image.shape
+        shape.append([height, width])
+        if i > 0:
+            assert shape[i] == shape[i - 1]
+    height, width = shape[0]
+    stride_w = stride_h = 40
+    num_w = 1 + (width - img_w) // stride_w
+    num_h = 1 + (height - img_h) // stride_h
+    assert (scale_list is not None)
+    total_images = np.zeros([len(images) * num_w * num_h, img_h, img_w, nb_channel])  # np.shape:(rows,cols)
+    total_labels = np.zeros([len(images) * num_w * num_h, img_h, img_w, nb_class])
+    for i in range(len(images)):
+        image = cv2.imread(os.path.join(images_path, images[i]))
+        label = np.load(os.path.join(labels_path, labels[i]))
+        for j in range(num_h):
+            for k in range(num_w):
+                idx = 36 * i + j * 9 + k
+                start_y = 0 + j * stride_h
+                end_y = start_y + img_h
+                start_x = 0 + k * stride_w
+                end_x = start_x + img_w
+                total_images[idx, :, :, :] = image[start_y:end_y, start_x:end_x]
+                for c in range(nb_class):
+                    total_labels[idx, :, :, c] = (label == scale_list) * 1
+    # mean normalization
+    if mode == 'train':
+        print('loading train images')
+        mean = np.mean(total_images, axis=0)
+        np.save('get_load_train_image_mean.npy', mean)  # for test data normalization
+    elif mode == 'test':
+        print('loading test images')
+        mean = np.load('get_load_train_image_mean.npy')
+    total_images = (total_images - mean) / (np.max(total_images) - np.min(total_images))
+
+    return total_images, total_labels
---
+++ get_train.py
@@ -0,0 +1,43 @@
+from keras.callbacks import ModelCheckpoint
+
+import config as c
+import get_load
+import get_model
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+config = c.Config()
+
+train_x, train_y = get_load.get_data(config.train_images_path, config.train_labels_path, config.img_h, config.img_w,
+                                     config.num_cls, config.num_chl, config.scale_list)
+print('train_x size: ', np.shape(train_x))
+print('train_y size: ', np.shape(train_y))
+assert (train_y.shape[1] == config.img_h and train_y.shape[2] == config.img_w)
+
+if os.path.isdir(config.model_path):
+    pass
+else:
+    os.mkdir(config.model_path)
+
+model = get_model.upp_model(config.img_h, config.img_w, config.num_chl, config.num_cls)
+model_checkpoint = ModelCheckpoint(config.model_path + '/uppnet.hdf5', monitor='loss', save_best_only=False,
+                                   save_weights_only=False, verbose=1)
+hist = model.fit(x=train_x, y=train_y, batch_size=config.batch_size, epochs=config.num_epoch, verbose=1, shuffle=True,
+                 validation_split=float(config.val_rate), callbacks=[model_checkpoint], initial_epoch=0)
+
+plt.plot(hist.hist['acc'])
+plt.plot(hist.hist['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize hist for loss
+plt.plot(hist.hist['loss'])
+plt.plot(hist.hist['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
